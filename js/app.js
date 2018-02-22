9e6015ffa5cdde1309af33e8c932a0b9536facc3
Jimmy Chang <jimmyfchang94@gmail.com>
2018-02-21 21:21:30 -0800

    adds local storage functionality and has a separate html and js for the table

--- js/app.js
+++ js/app.js
@@ -21,6 +21,10 @@ function generateRandom() {
   return Math.floor(Math.random() * BusMall.allImages.length);
 }
 
+function randomLogo() {
+
+}
+
 //This function makes the first 3 pictures and also makes sure that none of the images are the same.
 function randomPic() {
   while(randomImage.length < 6) {
@@ -40,85 +44,93 @@ function randomPic() {
 }
 
 function generateChart() {
-  var x = [];
-  var y = [];
+  var picName = [];
+  var picClicks = [];
   var picColor = [];
   for (var i = 0; i < BusMall.allImages.length; i ++) {
-    x.push(BusMall.allImages[i].name);
-    y.push(BusMall.allImages[i].clicks);
+    picName.push(BusMall.allImages[i].name);
+    picClicks.push(BusMall.allImages[i].clicks);
     picColor.push('blue');
   }
   var ctx = document.getElementById('canvasClicks').getContext('2d');
   var graphOfPics = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: x,
+      labels: picName,
       datasets: [{
         label: 'Number of Clicks',
-        data: y,
+        data: picClicks,
         backgroundColor: picColor,
       }],
     },
-      options: {
-        legend: {
-          display: false
-        },
-        title: {
-          display: true,
-          position: 'top',
-          fontSize: 20,
-          text:'Number of times an image was chosen'
-        },
-        responsive: false,
-        scales: {
-          yAxes: [{
-            ticks: {
-              min: 0,
-              stepSize: 1.0,
-            }
-          }]
-        }
+    options: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        position: 'top',
+        fontSize: 20,
+        text:'Number of times an image was chosen'
+      },
+      responsive: false,
+      animation: {
+        duration: 1000,
+        easing: 'easeOutBounce'
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            min: 0
+          }
+        }]
       }
+    }
   });
 
-  var z = [];
+  var picPercent = [];
   var picColor2 = [];
   for (var i = 0; i < BusMall.allImages.length; i ++) {
-    z.push(Math.floor(BusMall.allImages[i].clicks / BusMall.allImages[i].shown * 100));
+    picPercent.push(Math.floor(BusMall.allImages[i].clicks / BusMall.allImages[i].shown * 100));
     picColor2.push('purple');
   }
   ctx = document.getElementById('canvasPercentage').getContext('2d');
   var graphOfPics2 = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: x,
+      labels: picName,
       datasets: [{
         label: 'Percentage Clicked',
-        data: z,
+        data: picPercent,
         backgroundColor: picColor2,
       }],
     },
-      options: {
-        fontSize: 15,
-        legend: {
-          display: false
-        },
-        title: {
-          display: true,
-          position: 'top',
-          fontSize: 20,
-          text: 'Percentage of Clicks versus Times shown' 
-        },
-        responsive: false,
-        scales: {
-          yAxes: [{
-            ticks: {
-              min: 0,
-              stepSize: 10
-            }
-          }]
-        }
+    options: {
+      fontSize: 15,
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        position: 'top',
+        fontSize: 20,
+        text: 'Percentage of Clicks versus Times shown' 
+      },
+      responsive: false,
+      scales: {
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: 100,
+            stepSize: 20
+          }
+        }]
+      },
+      animation: {
+        duration:1000,
+        easing: 'easeOutBounce'
       }
+    }
   });
 }
 
@@ -141,31 +153,40 @@ function handleClick(event) {
   if (numClicks > 24) {
     picSection.style.display = 'none';
     generateChart();
+    var allImagesStringify = JSON.stringify(BusMall.allImages);
+    localStorage.setItem('allImages', allImagesStringify);
   }
 }
 
 //This creates the image objects
-new BusMall('bag', 'img/bag.jpg');
-new BusMall('banana', 'img/banana.jpg');
-new BusMall('bathroom', 'img/bathroom.jpg');
-new BusMall('boots', 'img/boots.jpg');
-new BusMall('breakfast', 'img/breakfast.jpg');
-new BusMall('bubblegum', 'img/bubblegum.jpg');
-new BusMall('chair', 'img/chair.jpg');
-new BusMall('cthulhu', 'img/cthulhu.jpg');
-new BusMall('dog-duck', 'img/dog-duck.jpg');
-new BusMall('dragon', 'img/dragon.jpg');
-new BusMall('pen', 'img/pen.jpg');
-new BusMall('pet-sweep', 'img/pet-sweep.jpg');
-new BusMall('scissors', 'img/scissors.jpg');
-new BusMall('shark', 'img/shark.jpg');
-new BusMall('sweep', 'img/sweep.png');
-new BusMall('tauntaun', 'img/tauntaun.jpg');
-new BusMall('unicorn', 'img/unicorn.jpg');
-new BusMall('usb', 'img/usb.gif');
-new BusMall('water-can', 'img/water-can.jpg');
-new BusMall('wine-glass', 'img/wine-glass.jpg');
-
+function imagePopulation() {
+  if (localStorage.length > 0) {
+    var allImagesRetrieved = localStorage.getItem('allImages');
+    BusMall.allImages = JSON.parse(allImagesRetrieved);
+  } else {
+      new BusMall('bag', 'img/bag.jpg');
+      new BusMall('banana', 'img/banana.jpg');
+      new BusMall('bathroom', 'img/bathroom.jpg');
+      new BusMall('boots', 'img/boots.jpg');
+      new BusMall('breakfast', 'img/breakfast.jpg');
+      new BusMall('bubblegum', 'img/bubblegum.jpg');
+      new BusMall('chair', 'img/chair.jpg');
+      new BusMall('cthulhu', 'img/cthulhu.jpg');
+      new BusMall('dog-duck', 'img/dog-duck.jpg');
+      new BusMall('dragon', 'img/dragon.jpg');
+      new BusMall('pen', 'img/pen.jpg');
+      new BusMall('pet-sweep', 'img/pet-sweep.jpg');
+      new BusMall('scissors', 'img/scissors.jpg');
+      new BusMall('shark', 'img/shark.jpg');
+      new BusMall('sweep', 'img/sweep.png');
+      new BusMall('tauntaun', 'img/tauntaun.jpg');
+      new BusMall('unicorn', 'img/unicorn.jpg');
+      new BusMall('usb', 'img/usb.gif');
+      new BusMall('water-can', 'img/water-can.jpg');
+      new BusMall('wine-glass', 'img/wine-glass.jpg');
+  }
+}
+imagePopulation();
 //This runs the function for the first 3 pictures
 randomPic();
 
